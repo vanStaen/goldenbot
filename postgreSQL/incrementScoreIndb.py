a405add795cff35939552cba9ae00c0ce15ebb4a
vanStaen <clement.vanstaen@gmail.com>
2021-06-28 15:20:52 +0200

    score incrementation

--- postgreSQL/incrementScoreIndb.py
+++ postgreSQL/incrementScoreIndb.py
@@ -15,10 +15,19 @@ def incrementScoreIndb(author):
         user_score = cursor.fetchone()
 
         if user_score:
-            updateScoreforUserIndb(author.id, user_score)
+            print(f"> Increment score for user #id {author.id}")
+            print(f"Old score: {user_score[0]}")
+            user_score_incremented = user_score[0] + 1
+            postgreSQL_update_Query = "UPDATE users SET score=%s WHERE telegram_id='%s'"
+            cursor.execute(
+                postgreSQL_update_Query,
+                (user_score_incremented, author.id),
+            )
+            connection.commit()
+            print(f"New score: {user_score_incremented}")
         else:
-            print("No user found, add to db")
             insertNewUserIndb(author)
+            print("> New user, will be added to the user db")
 
     except (Exception, psycopg2.Error) as error:
         print("Error while incrementing score in database: ", error)
