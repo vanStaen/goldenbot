1dd67eccc3532e381e76d0d3e2b01611862cff7a
vanStaen <clement.vanstaen@gmail.com>
2021-06-28 11:44:45 +0200

    catch error in main.py

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ import telebot
 API_KEY = config("API_KEY")
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
+try:
 
-@bot.message_handler(commands=["help"])
-def send_welcome(message):
-    bot.reply_to(message, "Dude, how that thing is working?")
-
-
-@bot.message_handler(commands=["score"])
-def send_welcome(message):
-    bot.reply_to(message, "I don't know your score yet.")
+    @bot.message_handler(commands=["help"])
+    def send_welcome(message):
+        bot.reply_to(message, "Dude, how that thing is working?")
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "You wrote:" + message.text)
+    @bot.message_handler(commands=["score"])
+    def send_welcome(message):
+        bot.reply_to(message, "I don't know your score yet.")
 
+
+    @bot.message_handler(func=lambda message: True)
+    def echo_all(message):
+        bot.reply_to(message, "You wrote:" + message.text)
+
+except telebot.apihelper.ApiException as e:
+  if e.result.status_code == 403 or e.result.status_code == 400:
+   pass 
 
 bot.polling()
