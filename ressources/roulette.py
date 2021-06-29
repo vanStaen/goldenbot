1d21bca182edbef9a6b0c99568906622dd084382
vanStaen <clement.vanstaen@gmail.com>
2021-06-29 12:14:13 +0200

    create ressources roulette

---
+++ ressources/roulette.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_random_kinkograph():
+    contents = requests.get("http://kinkograph.com/random").json()
+    url = contents["url"]
+    print(url)
+    return url
+
+
+# RandomPic command
+# @bot.message_handler(commands=["roulette"])
+# def send_welcome(message):
+#    chat_id = message.chat.id
+#    url = get_random_kinkograph()
+#    bot.send_photo(chat_id=chat_id, photo=url)
