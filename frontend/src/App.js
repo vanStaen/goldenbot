1d6a1b291aa3c6629188e3737a67c20d316e68fa
vanStaen <clement.vanstaen@gmail.com>
2021-07-09 21:45:23 +0200

    more styling fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Tabs } from "antd";
 
 import { UserData } from "./components/UserData/UserData";
 import { MeetingAttendee } from "./components/MeetingAttendee/MeetingAttendee";
-import { isMobileCheck } from "../../helpers/checkMobileTablet";
+import { isMobileCheck } from "./helpers/checkMobileTablet";
 import { Login } from "./components/Login/Login";
 
 import "./App.css";
@@ -13,12 +13,14 @@ const { TabPane } = Tabs;
 const App = () => {
   const [hasAccess, setHasAccess] = useState(true);
 
+  const isMobile = isMobileCheck();
+
   return (
     <div className="App">
       <header className="App-header">
         {hasAccess ? (
           <div className="App-Container">
-          <Tabs defaultActiveKey="2" centered={false}>
+          <Tabs defaultActiveKey="2" centered={isMobile}>
             <TabPane tab="User in Database" key="1">
               <UserData />
             </TabPane>
