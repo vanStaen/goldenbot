19c9d2e9804bd39bb0d97e8152dc69f3656f9055
vanStaen <clement.vanstaen@gmail.com>
2021-07-12 14:27:26 +0200

    joined_date

--- postgreSQL/insertNewUserIndb.py
+++ postgreSQL/insertNewUserIndb.py
@@ -10,30 +10,38 @@ def insertNewUserIndb(author, message, new):
         connection = psycopg2.connect(**params)
 
         cursor = connection.cursor()
-        postgreSQL_insert_Query = "INSERT INTO users(username, activity, first_name, last_name, telegram_id, last_seen_date, last_seen_on) VALUES(%s, %s, %s, %s, %s, %s, %s);"
 
         print(f"> User '{author.username}' added to db")
 
         now = date.today()
         if new == True:
-            time_stamp = "never"
-            chat_title = "none"
+            postgreSQL_insert_Query = "INSERT INTO users(username, activity, first_name, last_name, telegram_id, joined_date ) VALUES(%s, %s, %s, %s, %s, %s);"
+            cursor.execute(
+                postgreSQL_insert_Query,
+                (
+                    author.username,
+                    1,
+                    author.first_name,
+                    author.last_name,
+                    author.id,
+                    now,
+                ),
+            )
         else:
-            time_stamp = now
-            chat_title = message.chat.title
+            postgreSQL_insert_Query = "INSERT INTO users(username, activity, first_name, last_name, telegram_id, last_seen_date, last_seen_on) VALUES(%s, %s, %s, %s, %s, %s, %s);"
+            cursor.execute(
+                postgreSQL_insert_Query,
+                (
+                    author.username,
+                    1,
+                    author.first_name,
+                    author.last_name,
+                    author.id,
+                    now,
+                    message.chat.title,
+                ),
+            )
 
-        cursor.execute(
-            postgreSQL_insert_Query,
-            (
-                author.username,
-                1,
-                author.first_name,
-                author.last_name,
-                author.id,
-                time_stamp,
-                chat_title,
-            ),
-        )
         connection.commit()
 
     except (Exception, psycopg2.Error) as error:
